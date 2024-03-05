367c10e96b2542710ab99b38700de2781b23d6e7
klaus-maria <63562466+klaus-maria@users.noreply.github.com>
2024-03-05 18:42:05 +0100

    created basic sockets/threads

--- app/src/main/java/com/example/se2_mnr/MainActivity.java
+++ app/src/main/java/com/example/se2_mnr/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.se2_mnr;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private TextView serverResponse;
+    private Button connectBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        userInput = findViewById(R.id.userInput);
+        serverResponse = findViewById(R.id.serverResponse);
+        connectBtn = findViewById(R.id.connectBtn);
+    }
+
+    /**
+     * called when "Abschicken" Button is clicked
+     * connects to "se2-submission.aau.at" and sends value in the input field (matrikelnummer)
+     * response is shown in serverResponse TextView
+     */
+    public void connect(){
+        new Thread(() -> {
+            try {
+                Socket TcpConnection = new Socket("se2-submission.aau.at", 20080);
+
+                BufferedWriter sendOut = new BufferedWriter(new OutputStreamWriter(TcpConnection.getOutputStream()));
+                String matrikelnummer = userInput.getText().toString();
+                sendOut.write(matrikelnummer);
+
+                BufferedReader readerIn = new BufferedReader(new InputStreamReader(TcpConnection.getInputStream()));
+                String received = readerIn.readLine();
+
+                runOnUiThread(() -> {serverResponse.setText(received);});
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
